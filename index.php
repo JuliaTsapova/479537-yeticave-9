7a3e6820ec98c2373c51b8d7c34ea60128550e4a
Julia Tsapova <12tsapova@gmail.com>
2019-04-20 17:52:56 +0700

    Пишем первую функцию

--- index.php
+++ index.php
@@ -51,6 +51,18 @@ $lots = [
     ]
 ];
 
+function format_price($price) {
+    $price = ceil($price);
+
+    if($price >= 1000) {
+        $price = number_format($price, 0, '', ' ');
+    }
+
+    $formatted_price = $price . ' ₽';
+
+    return $formatted_price;
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -124,7 +136,7 @@ $lots = [
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost"><?= $lot['price']; ?><b class="rub">р</b></span>
+                            <span class="lot__cost"><?= format_price($lot['price']); ?></span>
                         </div>
                         <div class="lot__timer timer">
                             12:23
